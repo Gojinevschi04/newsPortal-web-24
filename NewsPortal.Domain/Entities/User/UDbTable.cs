4b6c7d2d4848aef4303a357bb6076297102f8eea
Lena Chizil <159022742+lenachizil@users.noreply.github.com>
2024-04-04 11:46:01 +0300

    add connection string sql data source

--- NewsPortal.Domain/Entities/User/UDbTable.cs
+++ NewsPortal.Domain/Entities/User/UDbTable.cs
@@ -1,6 +1,6 @@
 using System;
-// using System.ComponentModel.DataAnnotations;
-// using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using NewsPortal.Domain.Enums;
 
 
@@ -8,29 +8,29 @@ namespace NewsPortal.Domain.Entities.User
 {
     public class UDbTable
     {
-        // [Key]
-        // [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        // [Required]
-        // [Display(Name = "Username")]
-        // [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters.")]
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters.")]
         public string Username { get; set; }
 
-        // [Required]
-        // [Display(Name = "Password")]
-        // [StringLength(50, MinimumLength = 8, ErrorMessage = "Password cannot be shorter than 8 characters.")]
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password cannot be shorter than 8 characters.")]
         public string Password { get; set; }
 
-        // [Required]
-        // [Display(Name = "Email Address")]
-        // [StringLength(30)]
+        [Required]
+        [Display(Name = "Email Address")]
+        [StringLength(30)]
         public string Email { get; set; }
 
-        // [DataType(DataType.Date)] 
+        [DataType(DataType.Date)] 
         public DateTime LastLogin { get; set; }
 
-        // [StringLength(30)] 
+        [StringLength(30)] 
         public string LasIp { get; set; }
 
         public URole Level { get; set; }
