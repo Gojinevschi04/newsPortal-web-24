ab1b496cacacb2a52360d5f5ae260679a9236b16
Patriyck <152425281+Patriyck@users.noreply.github.com>
2024-05-22 14:33:38 +0300

    Add a script for collapse menu

--- NewsPortal.BusinessLogic/SessionBL.cs
+++ NewsPortal.BusinessLogic/SessionBL.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using NewsPortal.BusinessLogic.DbModel;
 using NewsPortal.Domain.Entities.Post;
 
